3c8c72fe1a6222c09b648eb8deba667e2551be25
WonderFIeld <o.mildzikhov@mail.ru>
2020-08-10 16:42:35 +0300

    fix

--- in progress/hw30/frame.js
+++
@@ -1,8 +0,0 @@
-const text = document.querySelector('.frame_wrapper');
-
-window.addEventListener("message", function(event) {
-    text.innerHTML = event.data;
-    if (parseInt(event.data)) {
-        parent.postMessage(parseInt(event.data) + 1, "*")
-    }
-});
